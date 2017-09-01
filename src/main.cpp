4fbfc39ea928f4f02e12fd295abdd6b8eb41fd9a
Anshuman <anshu.av97@gmail.com>
2017-09-01 20:05:42 +0530

    git add .

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main(){
 	
-	cout << "init";
-
+	//Parent loop for our shell
+	while(true) {
+		cout << "myshell$ > ";		// Printing shell name in each iteration
+		string ipcmd;				// Input command string
+		cin >> ipcmd;				// Reading input command string
+		
+	}
+	
 return 0;
 }
