b828e35ce463cd2da5073dda5d568a61676c1d85
Anshuman Verma <anshu.av97@gmail.com>
2017-09-03 01:00:44 +0530

    Add main skeleton code

--- src/main.c
+++ src/main.c
@@ -7,13 +7,36 @@ int main() {
 	//Parent loop for our shell
 	while(1) {
 		printf("%s", "myshell$ > ");		// Printing shell name in each iteration
-		char ipcmd[100];					// Input command string
-		gets(ipcmd);						// Reading input command string
+		char ipcmd[100];								// Input command string
+		gets(ipcmd);										// Reading input command string
 		
-		if (strcmp(ipcmd, "pwd") == 0) {
+		// when input command is ls
+		if (strcmp(ipcmd, "ls") == 0) {
+			// code for ls
+		}
+		
+		// when input command is cd
+		else if (strcmp(ipcmd, "cd") == 0) {
+			// code for cd
+		}
+		
+		// when input command is mkdir
+		else if (strcmp(ipcmd, "mkdir") == 0) {
+			// code for mkdir
+		}
+		
+		// when input command is rmdir
+		else if (strcmp(ipcmd, "rmdir") == 0) {
+			// when code is rmdir
+		}
+		
+		// when input command is pwd
+		else if (strcmp(ipcmd, "pwd") == 0) {
 			char *path = print_working_directory();
 			puts(path);
 		}
+		
+		
 	}
 	
 	return 0;
