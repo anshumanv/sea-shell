e24c9bff0a3ff16baded5d1b23430c8e6a5dea06
Shivam Saboo <201652025@iiitvadodara.ac.in>
2017-09-02 10:16:46 +0000

    Added functionality to store previous path

--- src/changeDirectory.c
+++ src/changeDirectory.c
@@ -5,6 +5,16 @@
 
 //Global Variables
 char * currdir = NULL;
+char * prevDir = NULL;
+
+
+//Function to store previously opened path
+char * storePreviousDirectory(char * dir){
+	char * previous = (char * )malloc(100*sizeof(char));
+	strcpy(previous, dir);
+	return previous;
+}
+
 
 //Funtion to concat two strings by efficiently allocating required memory.
 char * concat(const char *s1, const char *s2) {
@@ -14,6 +24,7 @@ char * concat(const char *s1, const char *s2) {
 	return result;
 }
 
+//Changes the directory
 char * changeDir(char * args[]){
 	char * buff = (char *)malloc(100*sizeof(char));	//Buffer to send error message  if the directory doesn't exist.
 	char * temp = (char *)malloc(100*sizeof(char));	//Temporarily stores value of current directory.
@@ -21,7 +32,7 @@ char * changeDir(char * args[]){
 	temp = concat(temp, "/");
 	temp = concat(temp, args[1]);
 	//If directory doesn't exist, exit the function with error message
-	if(chdir(temp) == -1){
+	if(chdir(temp) == -1 && args[1] != "-"){
 		snprintf(buff, 100, "The directory %s doesn't exists", args[1]);
 		return buff;
 	}
@@ -30,13 +41,22 @@ char * changeDir(char * args[]){
 		currdir = "/home/ubuntu";
 		return currdir;
 	}
+	
+	//If '-' return previous directory
+	if(args[1] == "-"){
+		return prevDir;
+	}else{
+		prevDir = storePreviousDirectory(currdir);
+	}
+	
 	currdir = temp;	//If the given path is valid, update the currdir path.
 	return currdir;
 }
 
+//Tester main (need to initialize global variables)
 void main(){
 	currdir = (char *)malloc(100*sizeof(char));
 	currdir = "/home/ubuntu";
-	char * args[] = {"cd", "Desktop"};
-	printf("%s", changeDir(args));
+	prevDir = (char *)malloc(100*sizeof(char));
+	strcpy(prevDir, currdir);
 }
