42ff135a5fbbbdd44f624df2d9255230e1694356
Anshuman Verma <anshu.av97@gmail.com>
2017-09-04 18:45:26 +0530

    Update main.c to include ls

--- src/main.c
+++ src/main.c
@@ -1,41 +1,88 @@
+// importing in-built libraries
 #include <stdio.h>
 #include <string.h>
+#include<stdlib.h> 
+#include <sys/types.h> 
+#include <dirent.h> 
+#include <sys/stat.h> 
+#include <unistd.h> 
+#include <fcntl.h> 
+#include <sys/ioctl.h> 
+
+// importing self-made libs 
 #include "pwd.c"
+#include "ls.c"
+
+//Global Variables
+char * currdir = NULL;
+char * prevDir = NULL;
+
 
 int main() {
-	
+	currdir = (char *)malloc(100*sizeof(char));
+	currdir = "/home/ubuntu";
+	prevDir = (char *)malloc(100*sizeof(char));
+	strcpy(prevDir, currdir);
+	chdir(currdir);
 	//Parent loop for our shell
 	while(1) {
+		int r = 0;
 		printf("%s", "myshell$ > ");		// Printing shell name in each iteration
-		char ipcmd[100];								// Input command string
-		gets(ipcmd);										// Reading input command string
+		
+		// input command string
+		char * ipcmd = (char *)malloc(100 * sizeof(char));
+		
+		// input command name and params
+		char * cmdParams[2];
+		cmdParams[0] = "";
+		cmdParams[1] = "";
+		fgets(ipcmd, 100, stdin);
+		
+		// remove trailing whitespaces
+		ipcmd = strtok(ipcmd, "\n");
+		int j;
+		for( j = 0; j<strlen(ipcmd); j++){
+			if (ipcmd[j] == ' '){
+				r = 1;
+			}
+		}
+		char *token = strtok(ipcmd, " ");
+		
+		// storing command name and params 
+		int i;
+		for(i = 0; i < r+1; i++){
+			cmdParams[i] = strdup(token);
+			token = strtok(NULL, " ");
+		}
 		
 		// when input command is ls
-		if (strcmp(ipcmd, "ls") == 0) {
-			// code for ls
+		if (strcmp(cmdParams[0], "ls") == 0) {
+			ls();
 		}
 		
 		// when input command is cd
-		else if (strcmp(ipcmd, "cd") == 0) {
+		else if (strcmp(cmdParams[0], "cd") == 0) {
 			// code for cd
+			//printf("%s\n", changeDir(cmdParams));
 		}
 		
 		// when input command is mkdir
-		else if (strcmp(ipcmd, "mkdir") == 0) {
+		else if (strcmp(cmdParams[0], "mkdir") == 0) {
 			// code for mkdir
 		}
 		
 		// when input command is rmdir
-		else if (strcmp(ipcmd, "rmdir") == 0) {
+		else if (strcmp(cmdParams[0], "rmdir") == 0) {
 			// when code is rmdir
 		}
 		
 		// when input command is pwd
-		else if (strcmp(ipcmd, "pwd") == 0) {
+		else if (strcmp(cmdParams[0], "pwd") == 0) {
 			char *path = print_working_directory();
 			puts(path);
 		}
 		
+		r = 0;
 		
 	}
 	
