6b92e94872379ed814ba83e3770fc8c283448a14
Khilan Ravani <khilanravani@gmail.com>
2017-09-07 20:42:40 +0530

    Change rmdir.c
    Fully functional code.
    Will add doc-strings shortly.

--- src/rmdir.c
+++ src/rmdir.c
@@ -1,26 +1,56 @@
 #include <stdio.h>
-#include<unistd.h>
 #include <stdlib.h>
-#include<string.h>
+#include <ftw.h>
+#include <dirent.h>
+#include <string.h>
 
-int main()
-{
-  int result;
-  char * dir = (char *)malloc(100 * sizeof(char));
-	
+int remove_directory(const char *path){
+   DIR *d = opendir(path);
+   size_t path_len = strlen(path);
+   int r = -1;
+   if (d){
+      struct dirent *p;
+      r = 0;
+      while (!r && (p=readdir(d))){
+          int r2 = -1;
+          char *buf;
+          size_t len;
 
-  printf("Enter the directory name: ");
-  fgets(dir,100,stdin);	//gets function giving warning, this will resolve
-	dir = strtok(dir, "\n");
-	
-  result = rmdir(dir);
-  if (result != 0)
-  {
-     perror("Could not remove directory");
-     exit(EXIT_FAILURE);
-  }
 
-  printf("Directory removed successfully\n");
+          if (!strcmp(p->d_name, ".") || !strcmp(p->d_name, "..")){
+             continue;
+          }
+          len = path_len + strlen(p->d_name) + 2; 
+          buf = malloc(len);
+          if (buf){
+             struct stat statbuf;
+             snprintf(buf, len, "%s/%s", path, p->d_name);
+             if (!stat(buf, &statbuf)){
+                if (S_ISDIR(statbuf.st_mode)){
+                   r2 = remove_directory(buf);
+                }
+                else{
+                   r2 = unlink(buf);
+                }
+             }
+             free(buf);
+          }
+          r = r2;
+      }
+      closedir(d);
+   }
+   if (!r){
+      r = rmdir(path);
+   }
+	return r;
+}
 
-  return 0;
+
+int main(int argc, char *argv[3]){
+    
+    int d=remove_directory(argv[1]);
+    
+    if(d==0)
+    printf("Directory Successfully removed\n");
+    return 0;
 }
