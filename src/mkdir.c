33c213f1f35677dd6088c530124e09bd3e07cbad
amany9000 <201651007@iiitvadodara.ac.in>
2017-09-09 15:13:33 +0530

    Made mkdir functional from main.c

--- src/mkdir.c
+++ src/mkdir.c
@@ -1,31 +1,10 @@
-
-#include <sys/stat.h>
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(int argc, char* argv[]){ 
-	if(strcmp(argv[1],"-v")==0){
-		const int dir_err = mkdir(argv[2] ,S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-		printf("created directory '%s' \n",argv[2]);	
-		if (-1 == dir_err){
-			printf("Error creating directory!\n");
-    		exit(1);
-		}	
+void makedir(char* argv) { 	
+	const int dir_err = mkdir(argv ,S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);	
+  if (-1 == dir_err) {
+			perror("Error creating directory!\n");
+    	    
+	} else {
+	        printf("File created successfully!\n");	
 	}
-	else if(strcmp(argv[1],"-p")==0){
-    	const int dir_err = mkdir(argv[2] ,S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);	
-		if (-1 == dir_err){
-			printf("Error creating directory!\n");
-    		exit(1);
-		}	
-	}	
-	else{
-		const int dir_err = mkdir(argv[1] ,S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);	
-		if (-1 == dir_err){
-			printf("Error creating directory!\n");
-    		exit(1);
-		}
-	}	
-	
-	return 0;
+     
 }
