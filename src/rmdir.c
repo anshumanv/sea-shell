d8c3c5fccd6091955481227ab54bc159972e7728
Khilan Ravani <khilanravani@gmail.com>
2017-09-11 12:32:58 +0530

    Doc-strings added

--- src/rmdir.c
+++ src/rmdir.c
@@ -1,42 +1,42 @@
-int remove_directory(const char *path){
-   DIR *d = opendir(path);
-   size_t path_len = strlen(path);
-   int r = -1;
-   if (d){
+int remove_directory(const char *path){   // "path"- parameter passing for directory to be removed
+   DIR *d = opendir(path);  // Move to the desired path to remove directory
+   size_t path_len = strlen(path);  // length of path
+   int r = -1;  // Status variable 
+   if (d){    // if the path exists
       struct dirent *p;
-      r = 0;
-      while (!r && (p=readdir(d))){
-          int r2 = -1;
-          char *buf;
+      r = 0;   // Status variable
+      while (!r && (p=readdir(d))){    //  execute the loop until the directory isn't empty 
+          int r2 = -1;  
+          char *buf; // storing and updating the current path in this variable
           size_t len;
 
 
-          if (!strcmp(p->d_name, ".") || !strcmp(p->d_name, "..")){
-             continue;
+          if (!strcmp(p->d_name, ".") || !strcmp(p->d_name, "..")){   // Check for "." & ".." files ; if found, continue
+             continue;  // don't remove the above files
           }
-          len = path_len + strlen(p->d_name) + 2; 
-          buf = malloc(len);
-          if (buf){
+          len = path_len + strlen(p->d_name) + 2;  //  
+          buf = malloc(len);  // Allocate memory at runtime
+          if (buf){          // If block statement for checking and deleting file/directory
              struct stat statbuf;
              snprintf(buf, len, "%s/%s", path, p->d_name);
              if (!stat(buf, &statbuf)){
-                if (S_ISDIR(statbuf.st_mode)){
-                   r2 = remove_directory(buf);
+                if (S_ISDIR(statbuf.st_mode)){    // Check for directory
+                   r2 = remove_directory(buf);    // remove directory
                 }
                 else{
-                   r2 = unlink(buf);
+                   r2 = unlink(buf);    // unlink command for deleting files
                 }
              }
-             free(buf);
+             free(buf);     // Free the allocated memory so as it can be used the next time the rmdir command is executed
           }
-          r = r2;
+          r = r2;  // updating the status variable
       }
-      closedir(d);
+      closedir(d);  //trace back to the original path
    }
 
    if (!r){
-      r = rmdir(path);
+      r = rmdir(path);    // if r!=0 or the outermost directory hasn't been deleted, remove it  
    }
-	return r;
+	return r;   // return the function status to main
 
 }
